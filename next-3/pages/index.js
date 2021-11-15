2826420f4ea3c49179b5b3fe194ad7d2c0901dd0
Albart Jose <albartbtme@gmail.com>
2021-11-15 20:57:15 +0530

    Next 3-1

---
+++ next-3/pages/index.js
@@ -0,0 +1,10 @@
+import styles from "./home.module.css";
+const HomePage = () => (
+  <>
+    <div className={styles.head}>Home Page</div>
+    <div className={styles.welcome}>
+      <img src="/welcome-my-page.jpg"></img>
+    </div>
+  </>
+);
+export default HomePage;
