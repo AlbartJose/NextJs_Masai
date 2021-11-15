a2f7be4460717f2814f0740af58ef90436f3ac66
Albart Jose <albartbtme@gmail.com>
2021-11-15 21:06:52 +0530

    Next 3-2 modified from 3-1

--- next-3/pages/index.js
+++ next-3/pages/index.js
@@ -1,9 +1,10 @@
 import styles from "./home.module.css";
+import Image from "next/image";
 const HomePage = () => (
   <>
     <div className={styles.head}>Home Page</div>
     <div className={styles.welcome}>
-      <img src="/welcome-my-page.jpg"></img>
+      <Image src="/welcome-my-page.jpg" width={800} height={500} />
     </div>
   </>
 );
